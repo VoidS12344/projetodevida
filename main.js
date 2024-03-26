40db182c69c120f830afeaade0ff12d13fa8bca1
VoidS2 <107724820+VoidS12344@users.noreply.github.com>
2024-03-26 07:48:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -19,8 +19,9 @@ const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo5 = new Date("2024-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4, tempoObjetivo5];
 
 
 function calculaTempo(tempoObjetivo) {
